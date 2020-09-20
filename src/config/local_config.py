7e73301b8487f95bcec8f7a230b270a8a6e4a5eb
jamshid <jamshidyerzakov@gmail.com>
2020-09-20 18:23:47 +0500

    add db config

---
+++ src/config/local_config.py
@@ -0,0 +1,12 @@
+# Data Base
+POSTGRES_SERVER = "localhost"
+POSTGRES_USER = "blog"
+POSTGRES_PASSWORD = "blog"
+POSTGRES_DB = "blog"
+
+# Email
+SMTP_TLS = True
+SMTP_PORT = 587
+SMTP_USER = "expressmarket.noreply@gmail.com"
+SMTP_PASSWORD = "$expressmarket12$"
+SMTP_HOST = "smtp.gmail.com"
